549d01ba63a2ec8312122bc16999b71f7ec4102c
wincyysh <141292574+wincyysh@users.noreply.github.com>
2026-05-01 23:58:39 +0200

    to do list

--- TodoList/src/App.js
+++ TodoList/src/App.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
+
+let id = 0;
+
+const INITIAL_TASKS = [
+  { id: id++, label: 'Walk the dog' },
+  { id: id++, label: 'Water the plants' },
+  { id: id++, label: 'Wash the dishes' },
+];
+
 export default function App() {
+  const [tasks, setTasks] = useState(INITIAL_TASKS);
+  const [newTask, setNewTasks] = useState('');
+
   return (
     <div>
       <h1>Todo List</h1>
       <div>
-        <input type="text" placeholder="Add your task" />
+        <input 
+          aria-label="Add new task"
+          type="text"
+          placeholder="Add your task" 
+          value={newTask}
+          onChange={(event) => {
+            setNewTasks(event.target.value);
+          }}
+        />
         <div>
-          <button>Submit</button>
+          <button
+            onClick={()=>{
+              setTasks(
+                tasks.concat({
+                  id: id++,
+                  label: newTask.trim(),
+                }),
+              );
+              setNewTasks('');
+            }}>
+            Submit
+          </button>
         </div>
       </div>
       <ul>
